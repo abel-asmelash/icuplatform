85312f77c75e9debf89255804730c31d481686a9
Abel Asmelash <abelasmelash07@gamil.com>
2026-05-29 20:19:46 +0200

    shadcn, install

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,11 @@
- import React from 'react'
- 
- const Home = () => {
-   return (
-     <h1 className='text-3xl text-red-200'> Welcome to Icu platform!</h1>
-   )
- }
- 
- export default Home
+import React from "react";
+
+const Home = () => {
+  return (
+    <>
+      <h1 className="text-3xl text-red-200">Welcome to Icu platform!</h1>
+    </>
+  );
+};
+
+export default Home;
