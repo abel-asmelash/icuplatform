fd5e990cfe90b7d1b48a7abb6b24aec0fd3628d6
Abel Asmelash <abelasmelash07@gamil.com>
2026-07-17 21:04:57 +0200

    Add language icon

--- components/navigation/navbar/index.tsx
+++ components/navigation/navbar/index.tsx
@@ -4,11 +4,12 @@ import Image from "next/image";
 import MobileNavigation from "./MobileNavigation";
 import { auth } from "@/auth";
 import UserAvatar from "@/components/UserAvatar";
-
+import { Button } from "@/components/ui/button";
+import { Languages } from "lucide-react";
 const Navbar = async () => {
   const session = await auth();
   return (
-    <nav className="flex items-center justify-between fixed z-50 w-full gap-5 p-6 bg-white dark:bg-gray-900 shadow-md sm:px-12">
+    <nav className="flex items-center justify-between fixed z-50 w-full gap-3 px-4 py-4 bg-white dark:bg-gray-900 shadow-md sm:gap-5 sm:px-12 sm:py-6">
       <Link href="/" className="flex shrink-0 items-center gap-3">
         <div className="flex h-12 w-12 shrink-0 items-center justify-center rounded-full bg-primary-500 shadow-lg">
           <Image
@@ -30,7 +31,10 @@ const Navbar = async () => {
         </div>
       </Link>
 
-      <p className="hidden truncate sm:block">Language Section</p>
+      <Button variant="outline" size="icon" className="shrink-0">
+        <Languages className="h-[1.2rem] w-[1.2rem]" />
+        <span className="sr-only">Change language</span>
+      </Button>
 
       <div className="flex shrink-0  items-center gap-3 sm:gap-5">
         <Theme />
